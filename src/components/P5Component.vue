0ab784f29f6de61e54990009974ec3b38020b759
joker <jian_zh@foxmail.com>
2022-04-27 19:38:14 +0800

    fix

--- src/components/P5Component.vue
+++ src/components/P5Component.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="p5-container" ref="p5"></div>
 </template>
+<script lang="ts">
+import { PropType, defineComponent } from "vue";
 
-<script>
 import p5 from "p5";
-function distinct(arr) {
+function distinct(arr: any) {
   return Array.from(new Set(arr));
 }
 const initialEvents = [
@@ -28,24 +29,32 @@ const initialEvents = [
   "touchMoved",
   "touchEnded",
   "windowResized",
-];
-export default {
+] as string[];
+export default defineComponent({
+  props: {
+    additionalEvents: Object as PropType<string[]> | Object,
+  },
+  data() {
+    return { initialEvents: [] };
+  },
   computed: {
-    p5Events() {
-      const { additionalEvents } = this;
-      return Array.isArray(additionalEvents)
-        ? distinct(initialEvents.concat(additionalEvents))
-        : initialEvents;
+    p5Events(): string[] {
+      return initialEvents;
+    },
+  },
+  methods: {
+    distinct(arr: string[]): string[] {
+      return Array.from(new Set(arr));
     },
   },
   mounted() {
     this.$nextTick(() => {
-      new p5(sketch => {
+      new p5((sketch) => {
         this.$emit("sketch", sketch);
         for (const p5EventName of this.p5Events) {
           const vueEventName = p5EventName.toLowerCase();
           const savedCallback = sketch[p5EventName];
-          sketch[p5EventName] = (...args) => {
+          sketch[p5EventName] = (...args: string[]) => {
             if (savedCallback) {
               savedCallback(sketch, ...args);
             }
@@ -55,12 +64,5 @@ export default {
       }, this.$el);
     });
   },
-};
-</script>
-<script setup>
-defineProps({
-  additionalEvents: Object,
 });
 </script>
-
-<style></style>
